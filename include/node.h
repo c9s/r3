ff1800cd4a444dc0123b8749702366c1e70fa100
c9s <yoanlin93@gmail.com>
2014-05-15 10:10:34 +0800

    add include

---
+++ include/node.h
@@ -0,0 +1,18 @@
+/*
+ * node.h
+ * Copyright (C) 2014 c9s <c9s@c9smba.local>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef NODE_H
+#define NODE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+
+
+#endif /* !NODE_H */
---
+++ include/str.h
@@ -0,0 +1,12 @@
+/*
+ * str.h
+ * Copyright (C) 2014 c9s <c9s@c9smba.local>
+ *
+ * Distributed under terms of the MIT license.
+ */
+#ifndef STR_H
+#define STR_H
+
+char** split_route_pattern(char *pattern, int pattern_len);
+
+#endif /* !STR_H */
