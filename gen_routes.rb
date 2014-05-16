6c5210c06ef3ff154c8da40cc44903210b8e9884
c9s <yoanlin93@gmail.com>
2014-05-16 21:43:54 +0800

    update benchmark page

--- gen_routes.rb
+++ gen_routes.rb
@@ -2,5 +2,6 @@
 arr    = ["foo", "bar", "baz", "qux", "quux", "corge", "grault", "garply"]
 paths  = arr.permutation(3).map { |a| "/#{a.join '/'}" }
 paths.each do |path|
-    puts "r3_tree_insert_path(n, \"#{path}\", NULL);"
+    # puts "r3_tree_insert_path(n, \"#{path}\", NULL);"
+    puts path
 end
