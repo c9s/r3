1d2eb53565e771f039cb6c2320ecb5d1f6fb99e4
c9s <cornelius.howl@gmail.com>
2014-05-20 16:21:05 +0800

    delete config.h since it's autogenerated

--- config.h
+++ config.h
@@ -60,9 +60,6 @@
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
 
-/* Define to 1 if the system has the type `_Bool'. */
-#define HAVE__BOOL 1
-
 /* Define to the sub-directory in which libtool stores uninstalled libraries.
    */
 #define LT_OBJDIR ".libs/"
