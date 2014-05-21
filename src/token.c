82ea09e146fc686ba11860902d7760a70201618d
c9s <yoanlin93@gmail.com>
2014-05-21 18:22:11 +0800

    when we free it, we know it's not NULL

--- src/token.c
+++ src/token.c
@@ -21,15 +21,13 @@ str_array * str_array_create(int cap) {
 }
 
 void str_array_free(str_array *l) {
-    if (l) {
-        for ( int i = 0; i < l->len ; i++ ) {
-            char * t = l->tokens[ i ];
-            if (t) {
-                zfree(t);
-            }
+    for ( int i = 0; i < l->len ; i++ ) {
+        char * t = l->tokens[ i ];
+        if (t) {
+            zfree(t);
         }
-        zfree(l);
     }
+    zfree(l);
 }
 
 bool str_array_is_full(str_array * l) {
