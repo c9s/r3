f6d2502896886f5d7c15e7cd8f7814aeb6244fcb
c9s <yoanlin93@gmail.com>
2014-05-21 22:08:25 +0800

    include jemalloc.h

--- src/zmalloc.c
+++ src/zmalloc.c
@@ -61,6 +61,7 @@ void zlibc_free(void *ptr) {
 #define realloc(ptr,size) tc_realloc(ptr,size)
 #define free(ptr) tc_free(ptr)
 #elif defined(USE_JEMALLOC)
+#include <jemalloc/jemalloc.h>
 #define malloc(size) je_malloc(size)
 #define calloc(count,size) je_calloc(count,size)
 #define realloc(ptr,size) je_realloc(ptr,size)
