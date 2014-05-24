2a25cfdf019d3b3f66013dae2f8b44206062a09e
c9s <yoanlin93@gmail.com>
2014-05-24 10:39:07 +0800

    use CK_VERBOSE

--- tests/check_tree.c
+++ tests/check_tree.c
@@ -419,7 +419,7 @@ int main (int argc, char *argv[]) {
         int number_failed;
         Suite *suite = r3_suite();
         SRunner *runner = srunner_create(suite);
-        srunner_run_all(runner, CK_NORMAL);
+        srunner_run_all(runner, CK_VERBOSE);
         number_failed = srunner_ntests_failed(runner);
         srunner_free(runner);
         return number_failed;
