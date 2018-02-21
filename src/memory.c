55de16402382e6a8fed3a87ffa6bb74567dea5c1
Martijn Otto <ottom@transip.nl>
2018-02-21 09:49:54 +0100

    Set the feature test macro before including the header to enable non-standard functions used by libr3

--- src/memory.c
+++ src/memory.c
@@ -19,6 +19,9 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
+
+#define _GNU_SOURCE
+
 #include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
