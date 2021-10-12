673bb2f6d9a126b6fbdb3b770d71f68fb5175d26
Björn Svensson <bjorn.a.svensson@est.tech>
2021-10-12 11:26:02 +0200

    Remove declarations for nonexisting functions in str_array.h

--- include/str_array.h
+++ include/str_array.h
@@ -24,16 +24,6 @@ typedef struct _str_array {
   R3_VECTOR(r3_iovec_t) tokens;
 } str_array;
 
-// str_array * str_array_create(int cap);
-
-bool str_array_slugs_full(const str_array * l);
-
-// bool str_array_tokens_full(const str_array * l);
-
-// bool str_array_resize(str_array *l, int new_cap);
-
-// bool str_array_append_slug(str_array * l, char * slug);
-
 bool str_array_append(str_array * l, const char * token, unsigned int len);
 
 void str_array_free(str_array *l);
@@ -41,8 +31,6 @@ void str_array_free(str_array *l);
 void str_array_dump_slugs(const str_array *l);
 
 void str_array_dump(const str_array *l);
-
-str_array * split_route_pattern(char *pattern, int pattern_len);
 
 #define str_array_fetch(t,i)  t->tokens[i]
 #define str_array_len(t)  t->len
